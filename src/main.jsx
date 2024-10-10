4c757c002fb88bee7b9a78e99f306910937df287
David Chay <yapperdark2@gmail.com>
2024-10-10 06:07:09 -0600

    Create a blank React Page

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Navbar from './components/navbar/Navbar'
+import Modal from './components/modal/Modal'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+  </StrictMode>,
+)

96a7860704cb5c61e01062e78b1909ce68bf5d95
David Chay <yapperdark2@gmail.com>
2024-10-10 07:43:27 -0600

    header section finished

--- src/components/headerSection/HeaderSection.jsx
+++ src/components/headerSection/HeaderSection.jsx
@@ -9,17 +9,36 @@ export default function HeaderSection() {
             <div className="container-hs">
                 <div className="image-section">
                     <div className="image-container">
-                    <img src={artImage1} alt="Arte 1" />
+                        <img src={artImage1} alt="Arte 1" />
                     </div>
                     <div className="image-container">
-                    <img src={artImage2} alt="Arte 2" />
+                        <img src={artImage2} alt="Arte 2" />
                     </div>
                 </div>
                 <div className="right-text-area">
-                    <h1 className="text-5xl font-bold mb-6">Compra y Vende Arte Digital</h1>
-                    <p className="text-lg mb-4">
-                        Somos la tienda por excelencia para la compra y venta de arte digital a nivel mundial.
-                    </p>
+                    <div className="introduction-text">
+                        <h1>Compra y Vende Arte Digital</h1>
+                        <p className="slogan-text">
+                            Somos la tienda por excelencia para la compra y venta de arte digital a nivel mundial.
+                        </p>
+                        <p className="little-bit-more">
+                            Contamos con:
+                        </p>
+                        <div className="stadistics">
+                            <div className="a-stadistic-for">
+                                <h2>999,000</h2>
+                                <p>Dibujos en venta</p>
+                            </div>
+                            <div className="a-stadistic-for">
+                                <h2>200</h2>
+                                <p>Artistas</p>
+                            </div>
+                            <div className="a-stadistic-for">
+                                <h2>10,000</h2>
+                                <p>Compradores</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
+import artLogo from './artevivo-logo.png';
 
 export default function Navbar() {
     return (
         <div>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a
-                        href="https://flowbite.com/"
-                        className="flex items-center space-x-3 rtl:space-x-reverse"
-                    >
-                        <img
-                            src="https://flowbite.com/docs/images/logo.svg"
-                            className="h-8"
-                            alt="Flowbite Logo"
-                        />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            Flowbite
-                        </span>
+                    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src={artLogo} className="h-8" alt="Flowbite Logo" />
                     </a>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button

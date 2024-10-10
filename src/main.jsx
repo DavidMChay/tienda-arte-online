ffbb72a87d268eeb62a239b55de8e42f19f36736
David Chay <yapperdark2@gmail.com>
2024-10-10 08:24:20 -0600

    Adding the Trending section

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navbar from './components/navbar/Navbar'
 import HeaderSection from './components/headerSection/HeaderSection'
+import TrendingSection from './components/trendingSection/TrendingSection'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
     <HeaderSection />
+    <TrendingSection />
   </StrictMode>,
 )

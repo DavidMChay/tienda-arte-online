f9cc8cfceb3fba05181864ae24decc2e20c41af5
David Chay <yapperdark2@gmail.com>
2024-10-11 07:40:57 -0600

    Add the App for the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DiscoverPage from './pages/DiscoverPage'; // Nueva página
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Ruta para la página principal */}
+        <Route path="/" element={<HomePage />} />
+        {/* Ruta para la página de catálogo "Descubre" */}
+        <Route path="/descubre" element={<DiscoverPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
